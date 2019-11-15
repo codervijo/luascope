9c0ba7b3066795c5c4781b5ce2b269451f153373
Vijo Cherian <codervijo@gmail.com>
2019-11-15 12:59:20 -0800

    Support multiple lua versions properly

--- lua51dock.sh
+++ lua51dock.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-#sudo docker build -t lua51 .
-sudo docker run -v $PWD:/usr/src/ --rm  -it lua51 lua /usr/src/scope.lua
+
+# Make sure b2b directory exists and is git clone of right project
+[ -d ./b2b ] || (echo "B2B GIT repo not found, exiting"; exit 77); [ "$?" -eq 77 ]  && exit 2
+
+CONTAINER=lua51
+
+# Make symlink to docker for this dev environment
+ln -sf b2b/apps/${CONTAINER}/Dockerfile .
+
+
+sudo docker build -t ${CONTAINER} .
+sudo docker run -v $PWD:/usr/src/ --rm  -it ${CONTAINER} lua /usr/src/scope.lua
--- lua53dock.sh
+++ lua53dock.sh
@@ -3,11 +3,11 @@
 # Make sure b2b directory exists and is git clone of right project
 [ -d ./b2b ] || (echo "B2B GIT repo not found, exiting"; exit 77); [ "$?" -eq 77 ]  && exit 2
 
-CONTAINER=lua52
+CONTAINER=lua53
 
 # Make symlink to docker for this dev environment
 ln -sf b2b/apps/${CONTAINER}/Dockerfile .
 
 
 sudo docker build -t ${CONTAINER} .
-sudo docker run -v $PWD:/usr/src/ --rm  -it ${CONTAINER} lua /usr/src/scope.lua
+sudo docker run -v $PWD:/usr/src/ --rm  -it ${CONTAINER} lua5.3 /usr/src/scope.lua
